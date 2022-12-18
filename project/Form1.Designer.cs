32bf3f8fc31c63464685967553558ca04f6a2484
Audrius Ivko <101082768+Audriusvilnius@users.noreply.github.com>
2022-12-18 13:41:39 +0200

    Add project files.

---
+++ project/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(295, -54);
+            this.panel1.Margin = new System.Windows.Forms.Padding(6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(965, 957);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label4.Location = new System.Drawing.Point(28, 534);
+            this.label4.Margin = new System.Windows.Forms.Padding(5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(221, 38);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "-";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label5.Location = new System.Drawing.Point(28, 630);
+            this.label5.Margin = new System.Windows.Forms.Padding(5);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(216, 38);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "-";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 747);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(229, 81);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(127, 30);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(103, 31);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            this.numericUpDown1.EnabledChanged += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Level";
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label11);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.numericUpDown1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(12, -1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(266, 835);
+            this.panel2.TabIndex = 9;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(28, 576);
+            this.label2.Margin = new System.Windows.Forms.Padding(5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(221, 38);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Caught";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(28, 674);
+            this.label3.Margin = new System.Windows.Forms.Padding(5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 38);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Lost ";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(28, 479);
+            this.label6.Margin = new System.Windows.Forms.Padding(5);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(216, 38);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Speed";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label7.Location = new System.Drawing.Point(28, 437);
+            this.label7.Margin = new System.Windows.Forms.Padding(5);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(221, 38);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "-";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label8.Location = new System.Drawing.Point(28, 94);
+            this.label8.Margin = new System.Windows.Forms.Padding(10);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(221, 67);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "-";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label9.Location = new System.Drawing.Point(28, 181);
+            this.label9.Margin = new System.Windows.Forms.Padding(10);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(221, 38);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "TOP SCORE";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.label10.Location = new System.Drawing.Point(28, 239);
+            this.label10.Margin = new System.Windows.Forms.Padding(10);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(221, 67);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "-";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label11.Location = new System.Drawing.Point(28, 326);
+            this.label11.Margin = new System.Windows.Forms.Padding(10);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(221, 38);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "TOP SPEED";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 846);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Typ me";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ project/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        Label[] L;
+        Random r = new Random();
+        public static int N = 10;
+        public static int speed = 1;
+        int typing_cor = 0;
+        int typing_count = 0;
+        int typing_count_er = 0;
+        int typing = 0;
+        int Top_speed = 0;
+        int Top_score = 0;
+        int Score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label4.Text = "" + typing;
+            label5.Text = "" + typing;
+            label7.Text = "" + speed;
+            button1.Enabled = false;
+            label4.ForeColor = Color.Gray;
+            label5.ForeColor = Color.Gray;
+            label7.ForeColor = Color.Gray;
+            label2.ForeColor = Color.Gray;
+            label3.ForeColor = Color.Gray;
+            label6.ForeColor = Color.Gray;
+            label8.ForeColor = Color.Green;
+            label10.ForeColor = Color.Crimson;
+            label4.Visible=false;
+            label5.Visible = false;
+            label7.Visible = false; 
+            label8.Visible = false;
+            label10.Visible = false;
+            label9.Enabled = false;
+            label11.Enabled = false;
+        }
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                if (L[i].Text == Convert.ToString(e.KeyCode))
+                {
+                    typing_cor++;
+                    typing_count_er++;
+                    L[i].Top = 0;
+                    L[i].Left = r.Next(0, panel1.Width - L[i].Width);
+                    L[i].Text = Convert.ToString((char)r.Next(65, 90));
+                }
+                label4.Text = "" + typing_cor;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            L = new Label[N];
+            Label[] labels = new Label[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                labels[i] = new Label();
+                labels[i].Parent = panel1;
+                L[i] = labels[i];
+            }
+            for (int i = 0; i < N; i++)
+            {
+                L[i].AutoSize = false;
+                L[i].BorderStyle = BorderStyle.None;
+                L[i].Font= new Font(this.Font, FontStyle.Bold);
+                L[i].Font = new Font("Tahoma", 12);
+                L[i].TextAlign = ContentAlignment.MiddleCenter;
+                L[i].ForeColor = Color.White;
+                L[i].FlatStyle = FlatStyle.System;
+                L[i].Width = 25;
+                L[i].Height = 25;
+                L[i].Top = 0;
+                L[i].Left = r.Next(0, panel1.Width - L[i].Width);
+                L[i].Text = Convert.ToString((char)r.Next(65, 90));
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                label7.Text = "" + speed;
+                L[i].Top += speed;
+                if (L[i].Top + L[i].Height >= panel1.Height)
+                {
+                    typing++;
+                    typing_count++;
+                    L[i].Top = 0;
+                    L[i].Left = r.Next(0, panel1.Width - L[i].Width);
+                    L[i].Text = Convert.ToString((char)r.Next(65, 90));
+                }
+                label5.Text = "" + typing;
+                if (typing_count >= 3)
+                {
+                    typing_count = 0;
+                    speed --;
+                }
+                if (typing_count_er >= 3)
+                {
+                    typing_count_er = 0;
+                    speed ++;
+                }
+                Top_score = (typing_cor - typing)*N;
+                if (Top_score > Score)
+                {
+                    Score = Top_score;
+                }
+                label8.Text = "" + Score ;
+                if (speed > Top_speed)
+                {  
+                    Top_speed = speed;
+                }
+                label10.Text = "" + Top_speed;
+            }
+        }
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            button1.Enabled = false;
+            numericUpDown1.Enabled = false;
+            label4.Visible = true;
+            label5.Visible = true;
+            label7.Visible = true;
+            label8.Visible = true;
+            label10.Visible = true;
+            label9.Enabled = true;
+            label11.Enabled = true;
+            label1.Enabled = false;
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            N = Convert.ToInt32(numericUpDown1.Value);
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+        }
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+        private void label9_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
